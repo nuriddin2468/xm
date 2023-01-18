ac35b43c638976817c7cbae037cae6af168bc6a4
Nuriddin Yuldashev <nuriddin_yuldashev@epam.com>
2023-01-18 11:35:00 +0500

    + storage added

--- README.md
+++ README.md
@@ -11,7 +11,6 @@ Project is about streaming unlimited images.
 1 - Lazy loaded images - For now, i am using here loading=lazy tag. But this solution works only on modern browsers. It would be better if i add another solutions here.<br>
 Solution is to add API Intersection Observer to detect when I should load image.<br/>
 2 - Improve Performance to infinitive scroll. It would be better if i add virtual scroll here too. In order to render only that parts what i need.<br/>
-3 - Storage. For now, I am saving <b>Favorite Photo</b> in Subject. Unfortunately, opensource api has no such functionality.<br/>
-One of the best solutions to it is to use localStorage, or sessionStorage. But there were written nothing about it in the task.<br/>
-4 - Need to add Not Found Page.
-5 - Change UI Interface.
+3 - Need to add Not Found Page.
+4 - Change UI Interface.
+5 - Try to not use <b>infinite scroll</b> as a component. Needed to implement directive. (in order to attach to every div)
--- src/app/features/services/photo.service.ts
+++ src/app/features/services/photo.service.ts
@@ -10,6 +10,7 @@ import { random } from '../../shared/utils/random';
 export class PhotoService {
 
   private endpoint = 'https://picsum.photos';
+  private storeKey = 'favorites';
 
   private favorites$ = new BehaviorSubject<Photo[]>([]);
 
@@ -26,6 +27,7 @@ export class PhotoService {
   }
 
   getFavorites(): Observable<Photo[]> {
+    this.setFromStore();
     return this.favorites$.asObservable();
   }
 
@@ -37,12 +39,24 @@ export class PhotoService {
     const favorites = this.favorites$.getValue();
     if (favorites.findIndex(item => item.id === photo.id) !== -1) return;
     this.favorites$.next([...favorites, photo]);
+    this.updateStore();
   }
 
   removePhotoFromFavorite(photo: Photo): void {
     const favorites = this.favorites$.getValue();
     this.favorites$.next(favorites.filter(favorite => favorite.id !== photo.id));
+    this.updateStore();
   }
+
+  private updateStore(): void {
+    localStorage.setItem(this.storeKey, JSON.stringify(this.favorites$.getValue()));
+  }
+
+ private setFromStore(): void {
+    const json = localStorage.getItem(this.storeKey);
+    if (!json) return;
+    this.favorites$.next(JSON.parse(json));
+ }
 
 
 }
